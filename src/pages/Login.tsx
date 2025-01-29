1d632d6e5a437774397d49fc98d1c4ef8357e3f7
jonayeds <jonayeeds@gmail.com>
2025-01-29 19:10:15 +0600

    added login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,41 @@
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../components/ui/form"
+import { Input } from "../components/ui/input"
+import { Button } from "../components/ui/button"
 
 const Login = () => {
+  const form = useForm({
+    defaultValues:{
+      username:""
+    }
+  })
+  const onSubmit =(values)=>{
+    console.log(values)
+  }
   return (
-    <div>Login</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input  {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    </div>
   )
 }
 
