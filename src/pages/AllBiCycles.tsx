8993e2ee8f612fdd083c887d34a6b6f32a79c83a
jonayeds <jonayeeds@gmail.com>
2025-04-04 11:32:41 +0600

    added search functionality

--- src/pages/AllBiCycles.tsx
+++ src/pages/AllBiCycles.tsx
@@ -1,11 +1,17 @@
+import { ChangeEvent, useState } from "react"
 import BiCycleCard from "../components/shared/BiCycleCard"
+import { Input } from "../components/ui/input"
 import { useGetAllBiCyclesQuery } from "../redux/features/bi-cycles/biCyclesApi"
 import { TBiCycle } from "../types/biCycles.types"
 
 const AllBiCycles = () => {
   const {data:allBiCycles, isLoading} = useGetAllBiCyclesQuery(undefined)
+  const [bicycles, setBicycles] = useState(allBiCycles)
+  const handleSearch = (e:ChangeEvent<HTMLInputElement>) =>{
+    setBicycles(allBiCycles.data?.filter((bicycle: TBiCycle) => bicycle.name.trim().split(" ").join("").toLowerCase().includes(e.target.value.trim().split(" ").join("").toLocaleLowerCase()) ))
+  }
   if(isLoading){
-    return <div className="flex h-screen  items-center" >
+    return <div className="flex h-screen justify-center items-center" >
       <h1 className="text-2xl">Loading...</h1>
     </div>
   }
@@ -13,9 +19,12 @@ const AllBiCycles = () => {
   return (
     <div className="pt-36">
       <h1 className="text-3xl text-center font-main" >All Bi-Cycles</h1>
+      <div className="flex justify-center mb-4 mt-8 ">
+        <Input className="max-w-[20vw] focus:shadow-lg shadow-sm duration-200 outline-none transition-all" placeholder="search..." onChange={handleSearch} />
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-max mx-auto">
         {
-          allBiCycles?.data.map((item: TBiCycle)=> <BiCycleCard key={item._id} biCycle={item} />)
+          bicycles.map((item: TBiCycle)=> <BiCycleCard key={item._id} biCycle={item} />)
         }
       </div>
     </div>
